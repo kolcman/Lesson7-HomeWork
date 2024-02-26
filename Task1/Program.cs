3faeb2934caf45833af57b08ec3c42b53bb667a1
Kolcman <m.kolcman@yandex.ru>
2024-02-26 12:56:56 +0300

    Lesson7:Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿//  Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+char[,] letters2d = {
+    {'a','b','c'},
+    {'d','e','f'},
+};
+
+string result = "";
+
+foreach (char letters in letters2d)
+{
+    result += letters; 
+}
+System.Console.Write(result);
