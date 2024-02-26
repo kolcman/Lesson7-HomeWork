73e56cd901dad82b2fdfc5e84102044b5ee75cd4
Kolcman <m.kolcman@yandex.ru>
2024-02-26 13:46:06 +0300

    Lesson7:Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// // Каким образом человек, который только пошел учиться, должен это решить?
+// // Какое это к черту обучение с нуля?
+
+public class Task3
+{
+    public static void Main(string[] args)
+    {
+        string input = "шалаш";
+        bool isPalindrome = IsPalindrome(input);
+        Console.WriteLine(isPalindrome ? "Да, но программа обучения у Вас ужас!" : "Нет, не рассказать про методы и просить их применить!");
+    }
+    public static bool IsPalindrome(string str)
+    {
+        string normalized = new
+        string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+        return normalized.SequenceEqual(normalized.Reverse());
+    }
+}
