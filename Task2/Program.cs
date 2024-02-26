702a0c8d129ca637b0c1d5a48b7225ab49f97cb1
Kolcman <m.kolcman@yandex.ru>
2024-02-26 13:09:52 +0300

    Lesson7:Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,4 @@
+﻿//  Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+string simpleString = "EEdfewWEvv3";
+System.Console.WriteLine(simpleString.ToLower());
